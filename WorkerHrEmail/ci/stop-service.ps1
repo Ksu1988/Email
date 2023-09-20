6434dbd64e9271a2575b1ddae88b83fb6e40cca6
Vitaliy Astakhov <SC11451@stada.ru>
2023-09-20 11:35:04 +0300

    fix

--- WorkerHrEmail/ci/stop-service.ps1
+++
@@ -1,9 +0,0 @@
-$ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
-Import-Module $ScriptDir\functions.psm1
-
-[string] $ServiceName = [Environment]::GetEnvironmentVariable($args[0])
-
-write-host "Service name $ServiceName"
-
-$stopCommand = "Stop-Service -Name $ServiceName -Force -PassThru;"
-TryManyWithTimeout -Command $stopCommand -Attempts 2
